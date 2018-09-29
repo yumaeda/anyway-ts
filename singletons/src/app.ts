689bb660e107ec0dfc1476bf052b9d0bab2cde1d
Yukitaka Maeda <yumaeda@gmail.com>
2018-09-29 15:59:08 +0900

    Add UrlUtility.ts

--- singletons/src/app.ts
+++ singletons/src/app.ts
@@ -10,3 +10,6 @@ document.write('<br>');
 document.write(String(DateTimeUtility.getDayOfWeek(2018, 9, 2)));
 document.write('<br>');
 document.write(DateTimeUtility.prependZeros(99, 5));
+
+import * as UrlUtility from './UrlUtility';
+document.write(UrlUtility.urlify('Les French Days, «Black Friday» à la française, commencent ce vendredi'));
