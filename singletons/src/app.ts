a57b0d40a5ade678c83ff4b638ee06887f898b20
Yukitaka Maeda <yumaeda@gmail.com>
2018-10-16 22:18:47 +0900

    Move DateTimeutility.ts to anyway-react

--- singletons/src/app.ts
+++ singletons/src/app.ts
@@ -1,16 +1,3 @@
-import * as DateTimeUtility from './DateTimeUtility';
-
-const weeks = DateTimeUtility.getWeekNames();
-for (const week of weeks) {
-    document.write(week + '<br>');
-}
-
-document.write(String(DateTimeUtility.getLastDateOfMonth(2018, 9)));
-document.write('<br>');
-document.write(String(DateTimeUtility.getDayOfWeek(2018, 9, 2)));
-document.write('<br>');
-document.write(DateTimeUtility.prependZeros(99, 5));
-
 import * as UrlUtility from './UrlUtility';
 document.write(UrlUtility.urlify('Les French Days, «Black Friday» à la française, commencent ce vendredi'));
 
